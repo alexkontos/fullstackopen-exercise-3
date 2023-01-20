c601b9094521d81db05483a6fc755ddf85c882a3
Alex Kontos <alex.a.kontos@gmail.com>
2023-01-20 14:05:24 +0200

    Complete backend step8

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan');
-app.use(morgan('tiny'))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :response-body'))
+
+morgan.token("response-body", (req, res) => {
+    if (req.method === "POST") {
+        return JSON.stringify(req.body)
+    } else {
+        return null
+    }
+})
 
 let persons = [
     {
